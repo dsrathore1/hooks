ad2ca38511cf66926c6aefbec07a7c6a4ffaec8a
DS <dsrathorebsw1234@gmail.com>
2023-03-06 13:05:19 +0530

    Use Effect Hook

--- src/Hooks/useEffect/useEffect.jsx
+++ src/Hooks/useEffect/useEffect.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Helmet} from "react-helmet";
+import './useEffectStyle.css'
 
-const useEffect = () => {
+const useEffectPage = () => {
+    const [size, setSize] = useState(window.screen.width);
+
+    const currentWindowSize = () => {
+        setSize(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', currentWindowSize);
+        return () => (window.removeEventListener("resize", currentWindowSize));
+    });
+
     return (
         <>
             <Helmet>
                 <title>Use Effect</title>
             </Helmet>
-            <h1 className="homePageContainer">Use Effect</h1>
+            <h1 className="effectHeader">The Size of the window is <span className="ueSpan">{size}</span>
+            </h1>
         </>
     );
 }
 
-export default useEffect;
+export default useEffectPage;
